a4263f099bd52c51c10f4e4ac35baa073a1df78c
@mariemilenalaipoon1995 <mariemilenalaipoon1995@gmail.com>
2024-05-29 16:52:22 +0400

    fix: Assurer que le menu déroulant se superpose correctement

--- src/components/Modal/Examples/DefaultModal.tsx
+++ src/components/Modal/Examples/DefaultModal.tsx
@@ -1,4 +1,7 @@
+import { Formik } from 'formik';
 import React from 'react';
+import { FormikForm, FormikSelect } from 'src/components/Formik';
+import { Vertical } from 'src/components/Layout';
 import { Button } from '../../Button/Button';
 import { Text } from '../../Text/Text';
 
@@ -21,11 +24,34 @@ export const DefaultModal = () => {
                 </Text>
               </Modal.Header>
               <Modal.Body>
+                <Formik initialValues={{ name: '' }} onSubmit={console.log}>
+                  {() => (
+                    <FormikForm>
+                      <Vertical alignItems="center">
+                        <Vertical gap={20} width="100%">
+                          <FormikSelect
+                            key={'name'}
+                            name={'name'}
+                            options={[
+                              'hello',
+                              'hello',
+                              'hello',
+                              'hello',
+                              'hello',
+                              'hello',
+                              'hello',
+                              'hello',
+                              'hello',
+                            ]}
+                          />
+                        </Vertical>
+                      </Vertical>
+                    </FormikForm>
+                  )}
+                </Formik>
                 <Text>
                   Cras mattis consectetur purus sit amet fermentum. Cras justo
-                  odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                  risus, porta ac consectetur ac, vestibulum at eros. Praesent
-                  commodo cursus magna, vel scelerisque nisl consectetur et.
+                  ctetur et.
                 </Text>
               </Modal.Body>
               <Modal.Footer>
