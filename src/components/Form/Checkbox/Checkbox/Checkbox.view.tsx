f2e19db0ea55096035952e412b0e0f73b1a22473
Monteiro Steed <steed.monteiro@gmail.com>
2024-09-03 15:55:45 +0200

    chore: release v0.7.17

--- src/components/Form/Checkbox/Checkbox/Checkbox.view.tsx
+++ src/components/Form/Checkbox/Checkbox/Checkbox.view.tsx
@@ -7,7 +7,8 @@ import { CheckSvg, IndeterminateSvg } from '../../../Svg';
 import { CheckboxViewProps } from './Checkbox.props';
 import { IconSizes, Sizes } from './Checkbox.style';
 import { Text } from '../../../Text/Text';
-import { Horizontal, Vertical } from 'src/components/Layout';
+import { Horizontal } from 'src/components/Layout/Horizontal/Horizontal';
+import { Vertical } from 'src/components/Layout/Vertical/Vertical';
 
 const CheckboxView: React.FC<CheckboxViewProps> = ({
   id,
