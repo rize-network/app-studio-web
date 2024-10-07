2b91f7b15d4f76b80eafc0f5d2cc5914abe92390
@mariemilenalaipoon1995 <mariemilenalaipoon1995@gmail.com>
2024-10-07 12:32:32 +0200

    Update Message.tsx

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,6 +1,4 @@
 export { MessageLayout } from './Message/Message.layout';
 export { MessageView } from './Message/Message.view';
-export { MessageProps } from './Message/Message.props';
-export { Message } from './Message/Message.type';
 
 export * from './Message/Message.store';
