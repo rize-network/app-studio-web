f67d0e9594d61ed48431c8a342d47aafacecc9bc
@mariemilenalaipoon1995 <mariemilenalaipoon1995@gmail.com>
2024-10-10 11:26:44 +0200

    feat: routing

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { ComponentsPage } from 'src/pages/components.page';
+import DocsPage from 'src/pages/docs/docs.page';
 import { HomePage } from 'src/pages/home.page';
 
 export const ComponentRouter = [
   { name: 'home', path: '/', element: <ComponentsPage /> },
   { name: 'home', path: '/home', element: <HomePage /> },
+  { name: 'home', path: '/home', element: <HomePage /> },
+  { name: 'doc', path: '/docs/:componentName?', element: <DocsPage /> },
 ];
